8fe9e73f8fc7ea26cf7486f756d30b1024f7990a
Guy Korland <gkorland@gmail.com>
2012-04-09 14:25:27 +0300

    focalserver

---
+++ focalserver.bat
@@ -0,0 +1,12 @@
+@echo off
+@rem This script starts the GigaSpaces JMX FocalServer
+
+rem Use local variables
+setlocal
+
+@call "%~dp0\setenv.bat"
+
+set CLASSPATH=%SPRING_JARS%;%GS_JARS%
+%JAVACMD% %GS_LOGGING_CONFIG_FILE_PROP% -cp %CLASSPATH% -Djava.security.policy=%POLICY% com.gigaspaces.jmx.focalserver.FocalServer "config/tools/focalserver.xml"
+
+endlocal
